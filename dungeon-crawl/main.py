0400e6fb11b21464fafd384fe286bc537c61905f
Chelsea Wetzel <chelseawetzel@Bradleys-Air.localdomain>
2022-10-20 18:55:50 -0400

    Create comments for steps in main

--- dungeon-crawl/main.py
+++ dungeon-crawl/main.py
@@ -30,7 +30,16 @@ room['kitchen'].n_to = room['treasure']
 room['treasure'].s_to = room['kitchen']
 
 
-# Main REPL loop for dungeon crawl
+# step 2 - prompt user to make a choice
+def get_user_direction():
+    direction = input("Choose your direction adventurer - n, s, e, w\n")
+    return direction
+
+# step 3 - if user quits game, show message
+
+# step 4 - compare users input and take them to location, if location is invalid, throw error
+
+# step 1 - display welcome message & describe game
 print(info_welcome())
 ask_name()
 
