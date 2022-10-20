5f8c40daf2aa6c41eec918e07288c544dff23a12
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2022-10-20 16:44:28 -0400

    Add welcome text function

---
+++ welcome.py
@@ -0,0 +1,7 @@
+def info_welcome():
+    print('Welcome to our version of the ultimate Dungeon Crawler.')
+    print('\n')
+    print('This game was created as a hacktoberfest project to get my wife and I into building something fun together...')
+    print('\n')
+    print('If you have any suggestions let us know... enjoy')
+    print('\n')
