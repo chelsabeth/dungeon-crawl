6b0d293b342f5d4e4dfc63fc0551092abf6cfbb3
Bradley Gilmore <bradleygilmore@Bradleys-Air.localdomain>
2022-10-21 19:30:19 -0400

    Create Player Model, map out starting properties

---
+++ dungeon-crawl/player.py
@@ -0,0 +1,35 @@
+class player:
+
+	def __init__(self, name):
+
+		# General
+
+		self.name = name
+
+
+		# Health Bar Related
+		# current_health
+		# max_health
+
+
+		# inventory (Gold included in inventory)
+		# level (Should EXP be seperate)
+
+		# Equiped (Weapons, armor, or anything else specifically equiped and not inside your inventory)... This could be a seperate model
+		# ATK level
+		# Armor level
+
+	def current_health:
+
+
+	# This will keep track off the current items that the player has equiped.
+	equipped = {
+		"main_hand": None,
+		"off_hand": None,
+		"helment": None,
+		"chest": None,
+		"hands": None,
+		"pants": None,
+		"shoes": None
+	}
+
