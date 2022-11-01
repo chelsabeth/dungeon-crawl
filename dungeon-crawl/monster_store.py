f48f9b99498222e7f687390cf0647a4b3dd947c0
Chelsea Wetzel <chelseawetzel@Bradleys-Air.localdomain>
2022-10-31 23:03:05 -0400

    Add names to monsters

--- dungeon-crawl/monster_store.py
+++ dungeon-crawl/monster_store.py
@@ -3,11 +3,11 @@ from monster import Monster
 
 # Declare monsters
 monsters = {
-    'undead': Monster("The spooky corpse stands before you, sword in hand. Defeat this hideous monster!", 3, 3, "medium"),
+    'undead': Monster("Undead", "The spooky corpse stands before you, sword in hand. Defeat this hideous monster!", 3, 3, "medium"),
 
-    'ooze': Monster("A green ooze squishes through the cracks of the wall. Defeat this sloppy monster before it consumes you.", 3, 2, "small"),
+    'ooze': Monster("Ooze", "A green ooze squishes through the cracks of the wall. Defeat this sloppy monster before it consumes you.", 3, 2, "small"),
 
-    'livingspirit': Monster("At first you see a haze in the distance, but then it starts to form in the shape of a person. This isn't the traditional ghosts you've fought before.", 3, 4, "medium"),
+    'livingspirit': Monster("Living Spirit", "At first you see a haze in the distance, but then it starts to form in the shape of a person. This isn't the traditional ghosts you've fought before.", 3, 4, "medium"),
 
-    'pumpkinhead': Monster("You see a man approaching you, but as he gets closer you realize he has a pumpkin for a head. It's time to make a jack-o-lantern out of him!", 5, 5, "large", ["5 gold", "pumpkin muffin recipe", "great sword"], ["pumpkin muffin recipe", "5 gold"])
+    'pumpkinhead': Monster("Pumpkin Head", "You see a man approaching you, but as he gets closer you realize he has a pumpkin for a head. It's time to make a jack-o-lantern out of him!", 5, 5, "large", ["5 gold", "pumpkin muffin recipe", "great sword"], ["pumpkin muffin recipe", "5 gold"])
 }
