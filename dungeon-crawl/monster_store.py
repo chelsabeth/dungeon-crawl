11f399051cae5acb4a1a9f9747e874e4bc8cfe16
Chelsea Wetzel <chelseawetzel@Bradleys-Air.localdomain>
2022-10-31 23:00:54 -0400

    Add items to monsters

--- dungeon-crawl/monster_store.py
+++ dungeon-crawl/monster_store.py
@@ -9,5 +9,5 @@ monsters = {
 
     'livingspirit': Monster("At first you see a haze in the distance, but then it starts to form in the shape of a person. This isn't the traditional ghosts you've fought before.", 3, 4, "medium"),
 
-    'pumpkinhead': Monster("You see a man approaching you, but as he gets closer you realize he has a pumpkin for a head. It's time to make a jack-o-lantern out of him!", 5, 5, "large")
+    'pumpkinhead': Monster("You see a man approaching you, but as he gets closer you realize he has a pumpkin for a head. It's time to make a jack-o-lantern out of him!", 5, 5, "large", ["5 gold", "pumpkin muffin recipe", "great sword"], ["pumpkin muffin recipe", "5 gold"])
 }
